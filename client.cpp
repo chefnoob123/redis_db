e369191ea46e5236b94d77c728c554cdd34e973e
chefnoob123 <navitmathurindia@gmail.com>
2025-11-18 01:19:38 +0530

    nov 18 work

--- client.cpp
+++ client.cpp
@@ -1,4 +1,6 @@
 #include <arpa/inet.h>
+#include <cassert>
+#include <cstdint>
 #include <errno.h>
 #include <netinet/ip.h>
 #include <stdint.h>
@@ -8,12 +10,82 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
 static void die(const char *msg) {
   int err = errno;
   fprintf(stderr, "[%d] %s\n", err, msg);
   abort();
 }
 
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    // this is a single attempt to write which could lead to short reads
+    assert((ssize_t)rv <= n); // sanity check so that read never return more
+                              // bytes than it is supposed to read
+    n -= (size_t)rv; // subtract the number of bytes we still need to read from
+                     // those that were read
+    buf += rv;       // moves the buffer pointer {prevents overwriting}
+  }
+  return 0;
+}
+static int32_t write_all(int fd, char *buf,
+                         size_t n) { // n: number of bytes to actually write
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    // this is a single attempt to write which could lead to short writes
+    assert((ssize_t)rv <= n);
+    n -= (ssize_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t query(int fd, const char *text) {
+  uint32_t len = (uint32_t)strlen(text);
+  if (len > k_max_msg) {
+    return -1;
+  }
+
+  // send request
+  char wbuf[4 + k_max_msg];
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], text, len);
+  if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+    return err;
+  }
+  // 4 bytes header
+  char rbuf[4 + k_max_msg];
+  errno = 0;
+  int32_t err = read_full(fd, rbuf, 4);
+  if (err) {
+    msg(errno == 0 ? "EOF" : "read() error");
+    return err;
+  }
+  memcpy(&len, rbuf, 4); // assume little endian
+  if (len > k_max_msg) {
+    msg("too long");
+    return -1;
+  }
+  // reply body
+  err = read_full(fd, &rbuf[4], len);
+  if (err) {
+    msg("read() error");
+    return err;
+  }
+  // do something
+  printf("server says: %.*s\n", len, &rbuf[4]);
+  return 0;
+}
 int main() {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
